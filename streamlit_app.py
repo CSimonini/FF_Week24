18ea29ec70cc51ebaf027cd8e715e2b67e5d9ec2
CSimonini <73425362+CSimonini@users.noreply.github.com>
2023-01-04 22:20:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,10 @@ sel_choice = st.sidebar.selectbox(
     )
 )
 
-st.write("show " + (sel_choice) + " in account")
+"""
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("show " + sel_choice + " in account")
+my_data_rows = my_cur.fetchall()
+st.dataframe(my_data_rows)
+"""
