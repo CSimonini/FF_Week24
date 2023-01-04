afe90525267ad8fbc3d7e804e24b1fc6b2a00929
CSimonini <73425362+CSimonini@users.noreply.github.com>
2023-01-04 20:48:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,7 @@ st.title('Snowflake Account Info App')
 
 st.text('Use this app to quickly see high-level info about your Snowflake account.')
 
-st.markdown(
-    """
-    <style>
-    .sidebar .sidebar-content {
-        background-color: #DEE0FB;
-        color: #ffffff
-}
-</style>
-""",
-    unsafe_allow_html=True,
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
 )
