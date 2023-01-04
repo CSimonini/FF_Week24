377ee432849046da8c2ff68540ab4b3c36b8943c
CSimonini <73425362+CSimonini@users.noreply.github.com>
2023-01-04 20:41:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,17 @@
 import streamlit
 
 streamlit.title('Snowflake Account Info App')
+
+streamlit.text('Use this app to quickly see high-level info about your Snowflake account.')
+
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-color: #DEE0FB;
+    color: #ffffff;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
