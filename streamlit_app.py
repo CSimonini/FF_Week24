8d42e12a22e8f072c738ab4bd3839689c1f6a3f1
CSimonini <73425362+CSimonini@users.noreply.github.com>
2023-01-05 21:24:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,26 @@ else:
     my_cur.execute(string_choice)
     my_data_rows = my_cur.fetchall()
     st.dataframe(my_data_rows)
+    
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(http://placekitten.com/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
